da7bc15c329675a7307559bb0474f0945b9444de
sanya2508 <1701sanya@gmail.com>
2021-01-31 05:42:02 +0530

    GET all articles done

--- app.js
+++ app.js
@@ -24,8 +24,23 @@ const articleSchema = {
 const Article = mongoose.model("Article", articleSchema);
 
 
-//TODO
+app.get("/articles", function(req,res){
+    Article.find(function(err, foundArticles){
+        if(!err){
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+        
+    })
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+
+
+
+
